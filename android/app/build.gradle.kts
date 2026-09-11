17f25f1659a384b486905b057a34da1ecdd0fa08
Zacarias Guilherme João <zacariasguilhermejoao@gmail.com>
2026-09-11 15:24:40 +0100

    targetSdk 36 + versionCode 2 (requisito Play Store)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,14 +15,14 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.dunta.taxi"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.dunta.taxi"
         minSdk = 26
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        targetSdk = 36
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     signingConfigs {
